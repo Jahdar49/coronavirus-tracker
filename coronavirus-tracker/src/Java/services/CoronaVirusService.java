3f40c6d5914ce77a61e28533dece53e187ad8ffd
Jahmel <Jahdar49@gmail.com>
2020-09-16 22:21:58 -0400

    Initial commit

---
+++ coronavirus-tracker/src/Java/services/CoronaVirusService.java
@@ -0,0 +1,10 @@
+package services;
+
+public class CoronaVirusService {
+
+    private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/archived_data/archived_time_series/time_series_19-covid-Confirmed_archived_0325.csv";
+
+    public void fetchVirusData() {
+
+    }
+}
---
+++ coronavirus-tracker/src/main/java/io/jahmel/coronavirustracker/controllers/HomeController.java
@@ -0,0 +1,30 @@
+package io.jahmel.coronavirustracker.controllers;
+
+import io.jahmel.coronavirustracker.models.LocationStats;
+import io.jahmel.coronavirustracker.services.CoronaVirusDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    CoronaVirusDataService coronaVirusDataService;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        List<LocationStats> allStats = coronaVirusDataService.getAllstats();
+        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+        model.addAttribute("locationStats",allStats);
+        model.addAttribute("totalReportedCases",totalReportedCases);
+        model.addAttribute("totalNewCases",totalNewCases);
+        return "home";
+
+    }
+}
